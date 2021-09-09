9e064ae64f7e567b27851b272a63baf3e9012464
Ritz Ghever Libetario <yahshua.ecpvav@gmail.com>
2021-09-09 17:30:52 +0800

    Heroku deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,7 +54,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangogirls',
         'USER': 'ritzecpvav',
         'PASSWORD': '7489443837',
