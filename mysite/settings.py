22d78143a1704555e86910bf9ccf185e326b4972
Ritz Ghever Libetario <yahshua.ecpvav@gmail.com>
2021-09-09 17:06:46 +0800

    Added files for Heroku deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,13 +1,14 @@
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 SECRET_KEY = 'r$!g$r1xif&g*zn6i!x(0kvbrt%fe^1_@=p^803=apt=je%3pi'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 INSTALLED_APPS = [
@@ -53,8 +54,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'ritzecpvav',
+        'PASSWORD': '7489443837',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -90,3 +95,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
